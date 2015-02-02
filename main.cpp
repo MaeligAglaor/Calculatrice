46deb86ef7b04896ba62a636be44bed82be6d45c
MaeligAglaor <Takolepoulpe@gmail.com>
2015-02-02 17:29:15 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+
+int32_t add ( int32_t a, int32_t b){
+
+return a+b;
+
+}
+int64_t add ( int64_t a, int64_t b){
+
+return a+b;
+
+}
+float add ( float a, float b){
+
+return a+b;
+
+}
+double add ( double a, double b){
+
+return a+b;
+
+}
+
+
+
+int32_t sous ( int32_t a, int32_t b){
+
+return a-b;
+
+}
+int64_t sous ( int64_t a, int64_t b){
+
+return a-b;
+
+}
+float sous ( float a, float b){
+
+return a-b;
+
+}
+double sous ( double a, double b){
+
+return a-b;
+
+}
+
+int32_t mul ( int32_t a, int32_t b){
+
+return a*b;
+
+}
+int64_t mul ( int64_t a, int64_t b){
+
+return a*b;
+
+}
+float mul ( float a, float b){
+
+return a*b;
+
+}
+double mul ( double a, double b){
+
+return a*b;
+
+}
+
+
+
+int32_t div ( int32_t a, int32_t b){
+
+return a/b;
+
+}
+int64_t div ( int64_t a, int64_t b){
+
+return a/b;
+
+}
+float div ( float a, float b){
+
+return a/b;
+
+}
+double div ( double a, double b){
+
+return a/b;
+
+}
+
+
+float mod ( float a, float b){
+
+return a%b;
+
+}
+double mod ( double a, double b){
+
+return a%b;
+
+}
+
+
+
+
+
+
+int main () {
+
+
+
+
+
+
+}
